244416b4e8fa810d8aeb6b581994e0c95299a5d1
AnaisRevellat <anaisrevellat@gmail.com>
2022-10-19 10:44:16 +0200

    working_on_resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,38 @@
 import "reflect-metadata";
-import { ApolloServer, gql } from "apollo-server";
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { ApolloServer } from "apollo-server";
 import datasource from "./utils";
-import WildersResolver from './resolvers/Wilders';
+import { buildSchema } from "type-graphql";
+import { WildersResolver } from "./resolvers/Wilders";
 
-const typeDefs = gql`
+const PORT = 5000;
 
-type Skill{
-	id: ID!
-	upvotes: [Upvote!]!
-	name: String!
+async function bootstrap(): Promise<void>{
+ 
+	const schema = await buildSchema({
+		resolvers: [WildersResolver], //	génère le schéma et y link les resolvers
+	});
+
+  // Create the GraphQL server
+  const server = new ApolloServer({
+    schema,    
+  });
+
+  // Start the server
+  const { url } = await server.listen(PORT);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+
+	try {
+		await datasource.initialize();
+		console.log("I'm connected!");
+	} catch (err) {
+		console.log("Dommage");
+		console.error(err);
+	}
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap();
 
-	type Upvote {
-	id: ID!
-	upvotes: Int!
-	skill: Skill!
-	wilder: Wilder!
-	}
 
-	type Wilder {
-		id: ID!
-		name: String!
-		upvotes: [Upvote!]!
-	}
 
-	type Query {
-		wilders: [Wilder]
-		wilder(id: ID): Wilder
-	}
 
-	type Mutation{
-		createWilder(name: String!): Wilder!
-	}
-`;
 
-const resolvers = {
-	Query: {
-		wilders: WildersResolver.findAll,
-		wilder: WildersResolver.find,		
-	},
-	Mutation: {
-		createWilder: WildersResolver.create,
-	}
-};
-
-// server
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	csrfPrevention: true,
-	cache: "bounded",
-	plugins: [
-		ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-	],
-});
-
-// The `listen` method launches a web server.
-server.listen(5000).then(
-	async () => {
-		console.log(`🚀  Server sarted `);
-
-		try {
-			await datasource.initialize();
-			console.log("I'm connected!");
-		} catch (err) {
-			console.log("Dommage");
-			console.error(err);
-		}
-	},
-	(err) => console.error(err)
-);
--- src/resolvers/Wilders.ts
+++ src/resolvers/Wilders.ts
@@ -1,100 +1,35 @@
-import datasource from "../utils";
-import { Request, Response } from "express";
+import { Arg, ID, Mutation, Query, Resolver } from "type-graphql";
 import { Wilder } from "../entities/Wilder";
+import datasource from "../utils";
 
-export default {
-	create: async (_: any, args: { name: string }): Promise<Wilder> => {
-		const repository = datasource.getRepository(Wilder);
+@Resolver()
+export class WildersResolver {
 
-		const id = await repository.query(
-			"INSERT INTO wilder(name) VALUES (?)",
-			[args.name]
-		);
-		const wilders = await repository.query(
-			"SELECT * FROM wilder WHERE id=?",
-			[id]
-		);
-		return wilders[0];
-	},
+	@Mutation(()=> Wilder)
+	async createWilder(@Arg("name") name: string): Promise<Wilder>{
+		return await datasource.getRepository(Wilder).save({ name });
+	}
 
-	findAll: async (): Promise<Wilder[]> => {
-		const repository = datasource.getRepository(Wilder);
-		return await repository.find({
-			relations: ["upvotes", "upvotes.skill"],
-		});
-	},
+	@Mutation(()=> Wilder)
+	async deleteWilder(@Arg("id", () => ID) id: number): Promise<Wilder>{
+		const wilderToDelete = await datasource
+		.getRepository(Wilder)
+		.findOne({where: {id}});				
+		return await datasource.getRepository(Wilder).remove(wilderToDelete);
+	}
 
-	find: async (
-		_: any,
-		args: { id: number }
-	): Promise<Wilder | null> => {
-		const wilderId = args.id;
-
-		// find 1 wilder by its ID
-		const data = await datasource
+	@Query(() => [Wilder])
+	async wilders(): Promise<Wilder[]> {
+		return await datasource
 			.getRepository(Wilder)
-			.findOneBy({ id: wilderId });
+			.find({ relations: ["upvotes", "upvotes.skill"] });
+	}
 
-		return data;
-	},
-	update: async (req: Request, res: Response): Promise<void> => {
-		const wilderId: string = req.params.wilderId;
-		const repository = datasource.getRepository(Wilder);
-
-		const wilder = await repository.findOne({
-			where: { id: Number(wilderId) },
-			relations: ["skills"],
-		});
-
-		if (wilder !== null) {
-			wilder.name = req.body.name;
-
-			const updatedWilder = await repository.save(wilder, {
-				reload: true,
+	@Query(() => Wilder, { nullable: true })
+	async wilder(@Arg("id", () => ID) id: number): Promise<Wilder | null> {
+		return await datasource
+			.getRepository(Wilder)
+			.findOne({where: { id }, relations: ["upvotes", "upvotes.skill"],
 			});
-			res.json(updatedWilder);
-		}
-	},
-	delete: (req: Request, res: Response): void => {
-		/**
-		 * 2 options:
-		 * - raw SQL → UPDATE
-		 * - TypeORM: find + save
-		 */
-		const wilderId = req.params.wilderId;
-		const repository = datasource.getRepository("Wilder");
-
-		// raw SQL
-		repository
-			.query("DELETE FROM wilder WHERE id=?", [wilderId])
-			.then(
-				() => {
-					res.json({ success: true });
-				},
-				(err) => {
-					console.error("Error when removing: ", err);
-					res.json({ success: false });
-				}
-			);
-
-		/* // find 1 wilder by its ID
-    // Google → typeorm get 1 item by ID
-    repository.findOneBy({ id: wilderId }).then(
-      (wilder) => {
-        repository.remove(wilder).then(
-          () => {
-            res.json({ success: true });
-          },
-          (err) => {
-            console.error("Error when removing: ", err);
-            res.json({ success: false });
-          }
-        );
-      },
-      (err) => {
-        console.error("Error when finding: ", err);
-        res.json({ success: false });
-      }
-    ); */
-	},
-};
+	}
+}
